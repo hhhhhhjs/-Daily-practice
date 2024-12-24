a1590a53300d69941385802b3eb7628ed926ca9c
hhhhhhjs <1512403978@qq.com>
2024-12-24 20:16:54 +0800

    feat: two numbers add

---
+++ 两数之和.js
@@ -0,0 +1,20 @@
+const twoNumbersAdd = (nums, target) => {
+    const map = new Map();
+    for(let i = 0; i < nums.length; i++){
+        const diff = target - nums[i]
+        if(map.has(diff)){
+            return [map.get(diff), i]
+        }
+        map.set(nums[i], i)
+        console.log(map)
+    }
+    return []
+}
+
+console.log(twoNumbersAdd([2, 7, 11, 15], 9)) // [0, 1]
+
+
+/** 如果直接使用数组的方法，复杂度太大；
+ * 使用map，可以将复杂度降低到O(n)‘
+ * map 使用场景：当需要快速频繁地查找元素时，可以使用map
+ */
