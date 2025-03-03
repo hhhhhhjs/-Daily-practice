174b8d0254e56ce3a91ec5824a66a09519bf092c
hhhhhhjs <1512403978@qq.com>
2025-03-03 15:41:58 +0800

    feat:deepclone

--- 前端常见算法/deepclone.js
+++ 前端常见算法/deepclone.js
@@ -1,13 +1,13 @@
 // 实现深拷贝
 const hobbymes = {
-    computer:1,
-    guitar:2,
-    sing:3
+    computer: 1,
+    guitar: 2,
+    sing: 3
 }
 const obj = {
-    name:'hj',
-    age:22,
-    hobby:hobbymes
+    name: 'hj',
+    age: 22,
+    hobby: hobbymes
 }
 
 // 1. 使用 json
@@ -17,28 +17,56 @@ const deepcloneJson = (obj) => {
 
 const result = deepcloneJson(obj)
 result.name = '哈哈'
-console.log('原对象',obj)
-console.log('新对象',result)
+// console.log('原对象',obj)
+// console.log('新对象',result)
 
 
 // 使用普通递归
 const deepclonerecursion = (obj) => {
     // 进行类型检查
-    if(typeof obj !== 'object'){
+    if (typeof obj !== 'object') {
         return false
     }
     const result = {}
-    for(let key in obj){
-       if(typeof obj[key] !== 'object'){
-        result[key] = obj[key]
-       }else {
-        result[key] = deepcloneJson(obj[key])
-       }
+    for (let key in obj) {
+        if (typeof obj[key] !== 'object') {
+            result[key] = obj[key]
+        } else {
+            result[key] = deepcloneJson(obj[key])
+        }
     }
     return result
 }
 
 const recursion = deepclonerecursion(obj)
 recursion.name = '嘻嘻'
-console.log('原对象', obj)
-console.log('新对象', recursion)
+// console.log('原对象', obj)
+// console.log('新对象', recursion)
+
+
+
+// 1. 使用信息隧道
+const clone = structuredClone(obj)
+clone.name = 'structured'
+// console.log(obj)
+// console.log(clone)
+
+
+// MessageChannel
+// postmessage 在发送消息的时候会对结构化消息进行结构化克隆，利用这一特性实现对象的深拷贝
+// postmessage 中只能放结构化的数据，例如 map,array,set.object, 不能放复杂的结构，比如 dom, Error, Promise
+// 这里 onmessage 是异步的，考虑封装为 promise
+
+const messagechannel = (obj) => {
+    return new Promise((resolve) => {
+        const { port1, port2 } = new MessageChannel()
+        port2.onmessage = (mes) => {
+            resolve(mes.data)
+        }
+        port1.postMessage(obj)
+    })
+}
+
+messagechannel(obj).then((res) => {
+    console.log(res)
+})
