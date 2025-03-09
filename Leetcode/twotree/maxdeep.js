ce23a17ee390aea0e7209cab784d7fca6f9e9aed
hhhhhhjs <1512403978@qq.com>
2025-03-09 09:44:44 +0800

    feat:二叉树的最大深度

---
+++ Leetcode/twotree/maxdeep.js
@@ -0,0 +1,11 @@
+// 二叉树的最大深度
+// 直接递归即可, 最后加上一是因为需要带上正在遍历的根节点
+const maxdeep = (root) => {
+    // 判断 root 是否为 null
+    if(!root){
+        return 0
+    }
+    const deepleft = maxdeep(root.left)
+    const deepright = maxdeep(root.right)
+    return Math.max(deepleft,deepright) + 1
+}
