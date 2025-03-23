c0cbfb1fbaa59a317cf2f41102498682ed0c6330
hhhhhhjs <1512403978@qq.com>
2025-03-23 10:38:30 +0800

    feat: 数组去重

--- 前端常见算法/数组去重.js
+++ 前端常见算法/数组去重.js
@@ -9,23 +9,37 @@ const useSet = (nums) => {
 
 const arrMethod = (nums) => {
     const result = nums.filter((item, index, arr) => {
-       return arr.indexOf(item) === index
+        return arr.indexOf(item) === index
     })
     return result
 }
 
 // 使用 reduce
 const useReduce = (nums) => {
-    const result =  nums.reduce((arr,item) => {
+    const result = nums.reduce((arr, item) => {
         return arr.includes(item) ? arr : [...arr, item]
-    },[])
+    }, [])
     return result
 }
 
 
-// 测试用例
-const arr = [1,1,2,2,3,3,4,4,5,5]
+// 使用 map
+const useMap = (nums) => {
+    const map = new Map()
+    for (let i = 0; i < nums.length; i++) {
+        if (map.has(nums[i])) {
+            continue
+        } else {
+            map.set(nums[i], 1)
+        }
+    }
+    return Array.from(map.keys())
+}
 
-console.log(useSet(arr))
-console.log(arrMethod(arr))
-console.log(useReduce(arr))
+    // 测试用例
+    const arr = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5]
+
+    console.log(useSet(arr))
+    console.log(arrMethod(arr))
+    console.log(useReduce(arr))
+    console.log(useMap(arr))
